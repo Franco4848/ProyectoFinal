8ec55ba65eb1cc0ecda7d09f7f3aafe58e7b1c3d
00nic <nicoenzogon@gmail.com>
2024-10-02 18:36:01 -0300

    Crear tabla 'usuario' en sql

--- BD.sql
+++ BD.sql
@@ -17,3 +17,17 @@ CREATE TABLE comentarios (
   FOREIGN KEY (publi_id) REFERENCES publi(id)
 
 );
+
+CREATE TABLE usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_completo VARCHAR(100) NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
+    fotoPerfil VARCHAR(100) NOT NULL,
+    presentacion VARCHAR(100) NOT NULL,
+    enlace VARCHAR(100) NOT NULL,
+    fechaNac DATE NULL,
+    ubicacion VARCHAR(100) NOT NULL,
+    mostrarSiNo VARCHAR(10) NOT NULL
+);
